e3dacb72caa42b13062ec41a87e0ae5bb50b33fa
nboaldin <nboaldin@gmail.com>
2018-08-24 14:09:59 -0500

    Forgot to delete the junk

--- dynamicSliderCss.js
+++ dynamicSliderCss.js
@@ -1,33 +1,11 @@
-var api = revapi7.on('revolution.slide.onchange', function(e, data) {
 
-  jQuery('.widget_revslider li').removeClass('grayscale-true').addClass('grayscale-' + api.find('li').attr('grayscale-color'));
-
-});
-
-////////////////////////////////
-
-var api = revapi7.on('revolution.slide.onchange', function(e, data) {
-  var current = data.currentslide[0];
-  var currentGrayValue = current.attributes.grayscale.value;
-
-  if (currentGrayValue === "true") {
-    console.log('Getting somewhere');
-    current.style.filter = 'grayscale(100%)';
-    current.style.animation = 'grayscale 2.5s 1s 1 forwards';
-  }
-
-});
-
-866-635-1330
-
-////////////////////////////////
 var api = revapi7;
 
 api.on('revolution.slide.onchange', function(e, data) {
   var current = data.currentslide[0];
   var currentIndex = data.slideIndex;
 
-  if (currentIndex === 1 || currentIndex === 2 || currentIndex === 3) {
+  if (currentIndex) {
     jQuery(current).removeClass('grayscale-true').addClass('grayscale-' + api.find('li').attr('grayscale-color'));
   }
 
